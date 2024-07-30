da52a8c75ad40d6d16651e0d986eafcc40e84c6f
Rure00 <asnjkd5193@gmail.com>
2024-07-30 19:22:00 +0900

    완료: frament 생성, 버튼 로직 추가

--- app/src/main/java/com/project/imagesearchingapp/MainActivity.kt
+++ app/src/main/java/com/project/imagesearchingapp/MainActivity.kt
@@ -5,16 +5,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.project.imagesearchingapp.databinding.ActivityMainBinding
+import com.project.imagesearchingapp.fragment.MyArchiveFragment
+import com.project.imagesearchingapp.fragment.SearchingFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private var fragmentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.main_fragment_container, SearchingFragment()).commitNow()
+        binding.searchingTabBtn.setOnClickListener {
+            if(fragmentIndex == 0) return@setOnClickListener
+            fragmentIndex = 0
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_fragment_container, SearchingFragment()).commitNow()
+        }
+        binding.archiveTabBtn.setOnClickListener {
+            if(fragmentIndex == 1) return@setOnClickListener
+            fragmentIndex = 1
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_fragment_container, MyArchiveFragment()).commitNow()
+        }
     }
+
+
 }
