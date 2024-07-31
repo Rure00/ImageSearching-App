95b46b10d697887cf62f45243f7f561b7fe9d730
Rure00 <asnjkd5193@gmail.com>
2024-07-31 14:53:49 +0900

    완료: Recycler view

--- app/src/main/java/com/project/imagesearchingapp/fragment/MyArchiveFragment.kt
+++ app/src/main/java/com/project/imagesearchingapp/fragment/MyArchiveFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.project.imagesearchingapp.R
+import com.project.imagesearchingapp.data.ImageData
 import com.project.imagesearchingapp.databinding.FragmentMyArchiveBinding
+import com.project.imagesearchingapp.recycler_view.ImageRvAdapter
 
 class MyArchiveFragment : Fragment() {
     private var _binding: FragmentMyArchiveBinding? = null
     private val binding get() = _binding!!
 
+    private val imageList = mutableListOf<ImageData>()
+    private val imageRvAdapter = ImageRvAdapter(imageList)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +24,14 @@ class MyArchiveFragment : Fragment() {
     ): View {
         _binding = FragmentMyArchiveBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.archiveRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = imageRvAdapter.apply { notifyItemRangeInserted(0, imageList.size) }
+        }
     }
 }
